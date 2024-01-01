8654cbf1db3a749c8d8f81a3e1bcebbc8ef097f6
wordhater <ryanlocke058@gmail.com>
2024-01-01 20:00:43 +1000

    updated readme

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-import
+import json
+import requests
+from time import sleep
+#load config.json
+
+with open('config.json') as json_data:
+    config = json.load(json_data)
+autoip = False
+
+print(config)
+if config["ip"] == "":
+    autoip = True
+else:
+    ip = config["ip"]
+    autoip = False
+# main loop
+while True:
+    if autoip:
+        
+#    url = f"https://dynamicdns.park-your-domain.com/update?host={config["host"]}&domain={config["domain"]}&password={config["password"]}&ip={ip}"
+#    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36', "Upgrade-Insecure-Requests": "1","DNT": "1","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Accept-Language": "en-US,en;q=0.5","Accept-Encoding": "gzip, deflate"}
+#    response = requests.get("",headers=headers)
--- readme.md
+++ readme.md
@@ -2,3 +2,17 @@
 
 ## Purpose:
 At least for namecheap, there is a lack of linux support for their DDNS. This was created to work around this issue
+
+## Config:
+
+Create a file named config.json in the path where your running the python script 
+
+### example contents:
+```
+{
+    "domain": "example.com",
+    "password": "long string of numbers/letters",
+    "ip": "0.0.0.0",
+    "host": "www"
+}
+```
