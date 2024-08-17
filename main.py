ea6fd72eb9711f6724285a2881d1e983ab952325
Wordhater <ryanlocke058@gmail.com>
2024-08-17 12:23:51 +1000

    updated compose to restart container

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import json
 import requests
-from sys import argv
 from time import sleep
 #load config.json
-print(argv)
+
 with open('config.json') as json_data:
     config = json.load(json_data)
 autoip = False
