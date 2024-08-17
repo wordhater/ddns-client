bbf8579a92ba4d7558dca6d48c731f8b52fbfe1b
Wordhater <ryanlocke058@gmail.com>
2024-08-17 10:28:06 +1000

    container builds and runs

--- setupenv.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# This script was created to make the setup process easier
-virtualenv --python=python3 .venv
-source .venv/bin/activate
-pip install requests
--- start.sh
+++ start.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
+
+virtualenv --python=python3 .venv
 source .venv/bin/activate
+pip install requests
 python3 main.py
